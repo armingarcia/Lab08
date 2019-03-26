cb04d0cfe06b4656f7cd3737f4e212c8478e7a49
armingarcia <47404545+armingarcia@users.noreply.github.com>
2019-03-26 15:54:10 -0500

    Add files via upload

--- src/BoardGame.java
+++ src/BoardGame.java
@@ -4,5 +4,4 @@ public class BoardGame {
 	protected java.util.LinkedHashMap<java.lang.String, Location>;
 	protected java.util.LinkedHashMap<java.lang.String, GamePiece>;
 	
-	public BoardGame() // minor change
-}
+	public BoardGame() 
